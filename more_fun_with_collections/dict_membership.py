73cf188d3fd015147ab2cf65e666e6be56c12a0e
Elijah Morishita <elijah.lm@gmail.com>
2020-10-12 17:39:28 -0500

    4th commit - test_in_dict_true - OK

--- more_fun_with_collections/dict_membership.py
+++ more_fun_with_collections/dict_membership.py
@@ -2,13 +2,23 @@
 Author: Elijah Morishita
 elmorishita@dmacc.edu
 10/12/2020
-
+This programs tests if a number entered in the parameters of in_dict matches the
+pre-determined set within the in_dict function
 """
 
 
-def in_dict():
-    pass
+def in_dict(my_dictionary):
+    """
+    This function compares my_dicitonary to the key in your_dictionary
+    :param my_dictionary:
+    :return: True or False depending on the parameter
+    """
+    your_dictionary = {1: "First", 2: "Second", 3: "Third", 4: "Fourth"}
+    if my_dictionary in your_dictionary:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
-    in_dict()
+    print(in_dict(4))
--- test/test_dict_membership.py
+++ test/test_dict_membership.py
@@ -4,11 +4,15 @@ from more_fun_with_collections.dict_membership import in_dict
 
 class Test_set(unittest.TestCase):
     def test_in_dict_true(self):
-        self.assertTrue(in_dict())
+        self.assertTrue(in_dict(3))
 
+    #  The first test will always be ignored because the 2nd test has the
+    #  same function name, I think you wanted us to name the 2nd test
+    #  test_in_dict_false, that way we could run 2x different tests
+    #  simultaneously
 
-   # def test_in_dict_true(self):
-   #     self.assertFalse(in_dict())
+    def test_in_dict_true(self):
+        self.assertFalse(in_dict(12))
 
 
 if __name__ == '__main__':
