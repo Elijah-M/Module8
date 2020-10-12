381d7973ed446988040a990b66d43e882f895817
Elijah Morishita <elijah.lm@gmail.com>
2020-10-12 16:45:54 -0500

    2nd commit - test_in_set_true - OK

--- test/test_set_membership.py
+++ test/test_set_membership.py
@@ -6,15 +6,10 @@ class Test_set(unittest.TestCase):
     def test_in_set_true(self):
         self.assertTrue(in_set(25))
 
-    #  I commented out the 2nd test because it has the same function name
-    #  as the 1st function, which means the definition will always change
-    #  to the 2nd version (the false version).  I think you meant to call
-    #  the second test - test_in_set_false - but just in case I still
-    #  named it test_in_set_true
+    #  Now I've un-commented out the 2nd test
 
-
-    #def test_in_set_true(self):
-    #    self.assertFalse(in_set(25))
+    def test_in_set_true(self):
+        self.assertFalse(in_set(25))
 
 
 if __name__ == '__main__':
