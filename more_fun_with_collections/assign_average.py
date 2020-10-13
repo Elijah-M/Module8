71adb74b44a63d44155c97da1a61d8eabd097bc2
Elijah Morishita <elijah.lm@gmail.com>
2020-10-12 19:37:53 -0500

    7th commit - Switch/Case, Key: 'A' OK

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -6,9 +6,23 @@ This program emulates a switch/case function
 """
 
 
-def switch_average():
-    pass
+def switch_average(case):
+    """
+    This function returns a value based on if its parameter is a valid key, if
+    the key doesn't exist then the string "Invalid Key!" is returned.
+    :param case:
+    :return: The value of a key based on the parameter, if the key doesn't
+    exist then the string "Invalid Key!" is returned
+    """
+    switch = {
+        'A': 1,
+        'B': 2,
+        'C': 3,
+        'D': 4,
+        'E': 5
+    }
+    return switch.get(case, "Invalid Key!")
 
 
 if __name__ == '__main__':
-    switch_average()
+    print(switch_average('A'))
