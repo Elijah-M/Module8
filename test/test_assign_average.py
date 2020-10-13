49f56fa21e101db38e6c9d9a49ac8e928eb6b03e
Elijah Morishita <elijah.lm@gmail.com>
2020-10-12 19:41:09 -0500

    11th commit - Switch/Case, Key: 'E' OK

--- test/test_assign_average.py
+++ test/test_assign_average.py
@@ -4,7 +4,7 @@ from more_fun_with_collections.assign_average import switch_average
 
 class TestSwitch(unittest.TestCase):
     def test_switch_average_return_value_A(self):
-        self.assertEqual(switch_average('D'), 4)
+        self.assertEqual(switch_average('E'), 5)
 
     def test_switch_average_return_value_BCDE(self):
         self.assertEqual(switch_average('B'), 2)
